fc061f0c228e51dc7b5ae6ee0aff6f8771ed8ed7
heejinsung <>
2021-10-14 14:33:28 +0900

    메뉴 추가/수정/삭제 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,43 @@
 // - [x] 메뉴가 추가되고 나면, input은 빈 값을 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+// TODO 메뉴 수정
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+// TODO 메뉴 삭제
+// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [] 총 메뉴 갯수를 count하여 상단에 보여준다.
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // form태그가 자동으로 전송되는걸 막아준다.
+  // 메뉴 총 갯수 count
+  const updateMenuCount = () => {
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount} 개`;
+  };
+
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    // 메뉴수정기능
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+      const updatedMenuName = prompt(
+        '메뉴명을 수정하세요!',
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+
+    // 메뉴 삭제
+    if (e.target.classList.contains('menu-remove-button')) {
+      if (confirm('정말 삭제하시겠습니까?')) {
+        e.target.closest('li').remove();
+        updateMenuCount();
+      }
+    }
+  });
+
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
@@ -19,7 +52,7 @@ function App() {
   const addMenuName = () => {
     if ($('#espresso-menu-name').value === '') {
       alert('값을 입력해주세요!');
-      return; // return 값을 작성함으로써 뒤에 코드가 실행안됨.
+      return;
     }
 
     const espressoMenuName = $('#espresso-menu-name').value;
@@ -42,11 +75,8 @@ function App() {
       'beforeend',
       menuItemTemplate(espressoMenuName)
     );
-    // 메뉴 총 갯수 count
-    // const 변수 = li 갯수를 카운팅해서 구하기 - html 클래스명이나 힌트들을 통해서 변수명 정하기
-    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-    $('.menu-count').innerText = `총 ${menuCount} 개`;
 
+    updateMenuCount();
     // input 빈값 초기화 (코드의 순서대로 실행되기때문에 가능)
     $('#espresso-menu-name').value = '';
   };
@@ -56,7 +86,7 @@ function App() {
     addMenuName();
   });
 
-  // 메뉴의 이름을 입력받는건
+  // 메뉴의 이름을 입력받기
   $('#espresso-menu-name').addEventListener('keypress', (e) => {
     if (e.key !== 'Enter') {
       return;
